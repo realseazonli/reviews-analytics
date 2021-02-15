184386cd4addd8951e141df55b85a3c8aaf91ca6
realseazonli <real.seazonli@gmail.com>
2021-02-15 22:35:49 +0800

    calc ave_length

--- read.py
+++ read.py
@@ -1,5 +1,9 @@
+
 data = []
 count = 0
+len_list = []
+total = 0
+result = 0
 with open('reviews.txt', 'r') as f :
     for line in f :
         data.append(line.strip())
@@ -7,3 +11,9 @@ with open('reviews.txt', 'r') as f :
         if count % 1000 == 0 :
             print(len(data))
 print('档案读取完了，总共有', len(data), '个留言')
+
+names = ['Tom', 'Simon', 'Mike', 'angerina']
+for review in data :
+    total = total + len(review)
+result = total / len(data)
+print(result)
